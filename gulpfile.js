cfcc2b3a5a74d79bdb947d90f1f2d8a6ec81d8d1
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-07-13 13:40:44 -0700

    Remove bower (#14)

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,7 @@
 
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
-var file = require('gulp-file');
 var { exec } = require('child_process');
-var pkg = require('./package.json');
 
 var argv = require('yargs')
 	.option('output', { alias: 'o', default: 'dist' })
@@ -34,20 +32,6 @@ gulp.task('lint', function() {
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failAfterError());
-});
-
-gulp.task('bower', function() {
-	var json = JSON.stringify({
-		name: pkg.name,
-		description: pkg.description,
-		homepage: pkg.homepage,
-		license: pkg.license,
-		version: pkg.version,
-		main: argv.output + '/' + pkg.name + '.js'
-	}, null, 2);
-
-	return file('bower.json', json, { src: true })
-		.pipe(gulp.dest('./'));
 });
 
 gulp.task('default', gulp.parallel('build'));
